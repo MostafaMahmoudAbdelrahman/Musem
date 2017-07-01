18efba066f027a3760b406ee5c3cacc28bdaaade
mostafa mahmoud <drshdrsh26@yahoo.com>
2017-07-01 18:00:26 +0200

    my vr app

---
+++ Assets/Udacity/Scripts/Coin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour 
+{
+    //Create a reference to the CoinPoofPrefab
+    public GameObject CoinPoofPrefab;
+    public AudioClip[] mySound;
+    public AudioSource soundSource;
+    public void OnCoinClicked() {
+        soundSource.clip = mySound[0];
+        soundSource.Play();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+this is first my design that i used in project
+to listen to any sound about any thing click in name box
+use unity 5.5.1f1 
+GVR sdk 1.03
+the scene name 'sss'
